5db15c8e7d87c1efe0fa695512bacb959d8f249f
shassaninia <xai.soft80@gmail.com>
2017-02-03 21:08:48 -0500

    Open connection before saving. Set mocha promis to ES6 promise.  Remove mocha require.

--- test/connection.js
+++ test/connection.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
-//connect to mongo database
-mongoose.connect('mongodb://localhost/testaroo');
+//ES6 Promises
+mongoose.Promise = global.Promise;
 
-mongoose.connection.once('open',function(){
-    console.log('Connection has been made');
-}).on('error',function(error){
-    console.log('Connection error: ',error);
+
+// Connect to the db before tests run
+before(function (done) {
+    //connect to mongo database
+    mongoose.connect('mongodb://localhost/testaroo');
+
+    mongoose.connection.once('open', function () {
+        console.log('Connection has been made');
+        done(); //the connection is complete
+    }).on('error', function (error) {
+        console.log('Connection error: ', error);
+    });
 });
